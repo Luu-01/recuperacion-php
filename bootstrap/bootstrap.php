64f2b522478409518676fe13bbcb9b20050d5267
Lu <luciaaragonvaello@gmail.com>
2026-05-29 21:35:13 +0200

    Register global CSRF middleware

--- bootstrap/bootstrap.php
+++ bootstrap/bootstrap.php
@@ -7,6 +7,7 @@ require_once __DIR__ . '/../app/Core/helpers/helper.php';
 require_once __DIR__ . '/autoload.php';
 
 use App\Core\Auth\Auth;
+use App\Http\Middlewares\CsrfMiddleware;
 use App\Models\Usuario;
 
 // Inicializar la sesión y las variables flash.
@@ -17,6 +18,9 @@ Auth::init(Usuario::class);
 
 // Crear la instancia del router.
 router();
+
+// Registrar middlewares globales.
+router()->addGlobalMiddleware(CsrfMiddleware::class);
 
 // Registrar las rutas de la aplicación.
 require __DIR__ . '/../routes/web.php';
